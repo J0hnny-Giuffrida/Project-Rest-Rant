bb6829871ea4ef36314b4cee8e10cee850c33ebd
Johnny Giuffrida <jcgiuffridaii@gmail.com>
2022-06-29 14:26:54 -0400

    Added places controller

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//Code to import Router
+app.use("/places", require("./controllers/places"));
 //Homepage Route
 app.get("/", (req, res) => {
   res.send("Hello World");
