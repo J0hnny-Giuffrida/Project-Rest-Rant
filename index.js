324d300fdf278ac369ba601326711e825f45bff3
Johnny Giuffrida <jcgiuffridaii@gmail.com>
2022-07-02 18:31:01 -0400

    Added home page view

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//Setup JSX
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
 //Code to import Router
 app.use("/places", require("./controllers/places"));
 //Homepage Route
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("home");
 });
 
 //404 Page Route
