debf621fe00708386f4d209d1f2becba9b11b7f8
Johnny Giuffrida <jcgiuffridaii@gmail.com>
2022-07-02 18:35:04 -0400

    Added 404 Page view

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 //404 Page Route
 app.get("*", (req, res) => {
-  res.status(404).send(`<h1>404 Page</h1>`);
+  res.status(404).render("error404");
 });
 
 app.listen(process.env.PORT, () => {
