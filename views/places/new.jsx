adbee605d67c0651f1b7f3b7fa406f8356e41a92
Johnny Giuffrida <jcgiuffridaii@gmail.com>
2022-07-04 17:08:05 -0400

    Formatted form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form () {
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                <form method="POST" action="/places">
+                    <div className="form-group">
+                        <label htmlFor="name">Place Name</label>
+                        <input class="form-control" id="name" name="name" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input class="form-control" type="url" id="pic" name="pic" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input class="form-control" id="city" name="city" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input class="form-control" id="state" name="state" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input class="form-control" id="cuisines" name="cuisines" required />
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                </form>
             </main>
         </Def>
     )
