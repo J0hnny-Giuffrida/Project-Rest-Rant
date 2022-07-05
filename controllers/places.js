657f41a61145380ef3cd2183c046f8cc4ebd6028
Johnny Giuffrida <jcgiuffridaii@gmail.com>
2022-07-05 19:34:00 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -25,4 +25,16 @@ router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
+//SHOW
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { place: places[id] });
+  }
+});
+
 module.exports = router;
